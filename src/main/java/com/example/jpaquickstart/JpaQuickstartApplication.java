08ac51d9aee5d2c90790eaaf117e3f96225ca602
gevorg.bagratunyan <gevorg.bagratunyan@softr.io>
2022-05-19 16:46:05 +0400

    innitial

---
+++ src/main/java/com/example/jpaquickstart/JpaQuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaQuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaQuickstartApplication.class, args);
+	}
+
+}
